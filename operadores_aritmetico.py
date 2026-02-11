b3a143108bcbbaa7220bbe5b5fad05628f7ecdfe
anatalie838-lab <anatalie838@gmail.com>
2026-02-11 16:53:35 -0300

    Add files via upload

---
+++ operadores_aritmetico.py
@@ -0,0 +1,12 @@
+import os 
+os.system("cls")
+numero = int(input("digite um numero"))
+
+
+antecessor = numero - 1 
+
+sucessor = numero +1 
+
+
+print("antecessor: ", antecessor)
+print("sucessor: ", sucessor)
